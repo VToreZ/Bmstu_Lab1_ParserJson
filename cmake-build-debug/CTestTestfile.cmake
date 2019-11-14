9e0431d2d8ab1b2b4984c35e76184a114a3b09e3
VToreZ <lumisitone@gmail.com>
2019-11-14 21:02:01 +0300

    init

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/rooted/Documents/l1-master
+# Build directory: /home/rooted/Documents/l1-master/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit_tests "/home/rooted/Documents/l1-master/cmake-build-debug/tests")
+set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/rooted/Documents/l1-master/CMakeLists.txt;56;add_test;/home/rooted/Documents/l1-master/CMakeLists.txt;0;")
